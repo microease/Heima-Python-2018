e2584cc567296e83431c67d04d4c0de57fca2b03
浮生若梦 <microease@163.com>
2019-01-28 16:19:49 +0800

    add

--- 15 flask框架/10-图书案例/test_author_book.py
+++ 15 flask框架/10-图书案例/test_author_book.py
@@ -1,14 +1,23 @@
 # coding=utf-8
 import unittest
-from autor_book import Author, db,app
+from autor_book import Author, db, app
 
 
 class DatabaseTest(unittest.TestCase):
     def setUp(self):
         app.testing = True
         app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://"
+        db.create_all()
 
     def test_add_author(self):
         # 测试添加作者的数据库操作
         author = Author(name="张", email="itcast@itcast.cn", mobile="13838384381")
         db.session.add(author)
+        db.session.commit()
+        result_author = Author.query.filter_by(name="zhang").first()
+        self.assertIsNone(result_author)
+
+    def tearDown(self):
+        # 在所有的测试执行之后，执行，通常用来进行清理操作
+        db.session.remove()
+        db.drop_all()
