a77dd963de736579300493f0f16163b28ea68e61
浮生若梦 <microease@163.com>
2019-02-23 18:01:22 +0800

    add

--- 15期/16 微信公众号/wechat.py
+++ 15期/16 微信公众号/wechat.py
@@ -1,10 +1,12 @@
 # coding:utf-8
-from flask import Flask, request, abort
+from flask import Flask, request, abort, render_template
 import hashlib
+import urllib2
 
 # 常量
 WECHAT_TOKEN = "huyankai"
-
+WECHAT_APPID = "A"
+WECHAT_SECRECT = "B"
 app = Flask(__name__)
 
 
@@ -33,6 +35,36 @@ def wechat():
         abort(403)
     else:
         return echostr
+
+
+@app.route("/wechat8000/index")
+def index():
+    # 让用户通过微信访问的网页视图
+    # 从微信服务器中拿去用户的资料数据
+    code = request.args.get("code")
+    if not code:
+        return u"缺失code参数"
+    url = ""
+    response = urllib2.urlopen(url)
+    # 获取响应体数据，微信返回的json数据
+    json_str = response.read()
+    resp_dict = json.loads(json_str)
+
+    # 提取access_token
+    if "errcode" in resp_dict:
+        return u"获取access_token失败"
+    access_token = resp_dict.get("access_token")
+    open_id = resp_dict.get("openid")
+    url = ""
+    response = urllib2.urlopen(url)
+    user_json_str = response.read()
+    user_dict_data = json.loads(user_json_str)
+
+    if "errcode" in user_dict_data:
+        return u"获取用户信息失败"
+    else:
+        # 将用户的数据填充到页面中
+        return render_template("index.html",user = user_dict_data)
 
 
 if __name__ == '__main__':
