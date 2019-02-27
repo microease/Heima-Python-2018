b2cffdd730cc19382267af3acbf1300b4ca03d12
浮生若梦 <microease@163.com>
2019-02-27 17:58:00 +0800

    add

--- 15期/17 爱家租房项目/ihome_python/manager.py
+++ 15期/17 爱家租房项目/ihome_python/manager.py
@@ -2,6 +2,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import redis
+from flask_session import Session
 
 app = Flask(__name__)
 
@@ -16,6 +17,10 @@ class Config(object):
     # redis
     REDIS_HOST = "127.0.0.1"
     REDIS_PORT = "6379"
+    SESSION_TYPE = "redis"
+    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
+    SESSION_USE_SIGNER = True
+    PERMANENT_SESSION_LIFETIME = 86400  # session数据的有效期 秒
 
 
 app.config.from_object(Config)
@@ -23,6 +28,8 @@ db = SQLAlchemy(app)
 
 # 创建redis连接对象
 redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
+# 利用flask_session，将session数据保存到redis中
+Session(app)
 
 
 @app.route("/index")
