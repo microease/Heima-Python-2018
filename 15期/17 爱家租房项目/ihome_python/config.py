ab93b92d7dce643047cc8ccf983c4dcf2f7d1834
浮生若梦 <microease@163.com>
2019-02-28 00:22:03 +0800

    add

---
+++ 15期/17 爱家租房项目/ihome_python/config.py
@@ -0,0 +1,31 @@
+# coding:utf-8
+class Config(object):
+    """配置信息"""
+    DEBUG = True
+    SECRET_KEY = "SFJSAKLFHASLKJHASKNDMSBN"
+    # 数据库
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/ihome_python"
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    # redis
+    REDIS_HOST = "127.0.0.1"
+    REDIS_PORT = "6379"
+    SESSION_TYPE = "redis"
+    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
+    SESSION_USE_SIGNER = True
+    PERMANENT_SESSION_LIFETIME = 86400  # session数据的有效期 秒
+
+
+class DevelopmentConfig(Config):
+    # 开发模式
+    DEBUG = True
+
+
+class ProductConfig(Config):
+    # 生产环境
+    pass
+
+
+config_map = {
+    "develop": DevelopmentConfig,
+    "product": ProductConfig
+}
--- 15期/17 爱家租房项目/ihome_python/manager.py
+++ 15期/17 爱家租房项目/ihome_python/manager.py
@@ -3,33 +3,33 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import redis
 from flask_session import Session
-
-app = Flask(__name__)
-
-
-class Config(object):
-    """配置信息"""
-    DEBUG = True
-    SECRET_KEY = "SFJSAKLFHASLKJHASKNDMSBN"
-    # 数据库
-    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/ihome_python"
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
-    # redis
-    REDIS_HOST = "127.0.0.1"
-    REDIS_PORT = "6379"
-    SESSION_TYPE = "redis"
-    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
-    SESSION_USE_SIGNER = True
-    PERMANENT_SESSION_LIFETIME = 86400  # session数据的有效期 秒
+from flask_wtf import CSRFProtect
+from config import config_map
 
 
-app.config.from_object(Config)
+# 工厂模式
+def create_app(config_name):
+    """
+    创建Flask的应用对象
+    :param config_name:string 配置模式的名字
+    :return:
+    """
+    app = Flask(__name__)
+    # 根据配置模式的名字获取配置参数的类
+    config_class = config_map.get(config_name)
+    app.config.from_object(config_class)
+    return app
+
+
+app = create_app("develop")
 db = SQLAlchemy(app)
 
 # 创建redis连接对象
 redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
 # 利用flask_session，将session数据保存到redis中
 Session(app)
+# 为Flask补充csrf防护
+CSRFProtect(app)
 
 
 @app.route("/index")
