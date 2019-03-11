43d600a6838db9eb8e71b166370b4a82633fd36a
浮生若梦 <microease@163.com>
2019-03-11 15:36:56 +0800

    add

--- 15期/17 爱家租房项目/ihome_python/ihome/api_1_0/passport.py
+++ 15期/17 爱家租房项目/ihome_python/ihome/api_1_0/passport.py
@@ -105,3 +105,11 @@ def login():
     session["mobile"] = user.mobile
     session["user_id"] = user.id
     return jsonify(errno=RET.OK,errmsg="登录成功")
+@api.route("/session",method=["GET"])
+def check_login():
+    name=session.get("name")
+    if name is not None:
+        return jsonify(errno=RET.OK,errmsg="true",data={"name":name})
+    else:
+        return jsonify(errno=RET.SESSIONERR,errmsg="false")
+    
