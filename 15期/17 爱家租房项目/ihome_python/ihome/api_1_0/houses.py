db3303d87b1c73da1f5939ffd7e680c9fea18550
浮生若梦 <microease@163.com>
2019-03-12 15:30:03 +0800

    add

---
+++ 15期/17 爱家租房项目/ihome_python/ihome/api_1_0/houses.py
@@ -0,0 +1,22 @@
+# coding:utf-8
+from . import api
+from ihome.utils.commons import login_required
+from flask import g, current_app, jsonify, request
+from ihome.utils.response_code import RET
+from ihome.utils.image_storage import storage
+from ihome.models import User, Area
+from ihome import db, constants
+
+
+@api.route("/areas", method=["GET"])
+def get_area_info():
+    # 获取城区信息
+    try:
+        area_li = Area.query.all()
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="数据库错误")
+    area_dict_li = []
+    for area in area_li:
+        pass
+    return jsonify(errno=RET.OK, errmsg="OK", data=area_dict_li)
