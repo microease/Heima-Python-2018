7bbb1ecceff2fa511432f851e7e464ad53c64a24
浮生若梦 <microease@163.com>
2019-03-12 16:47:39 +0800

    add

--- 15期/17 爱家租房项目/ihome_python/ihome/api_1_0/houses.py
+++ 15期/17 爱家租房项目/ihome_python/ihome/api_1_0/houses.py
@@ -5,7 +5,8 @@ from flask import g, current_app, jsonify, request
 from ihome.utils.response_code import RET
 from ihome.utils.image_storage import storage
 from ihome.models import User, Area
-from ihome import db, constants
+from ihome import db, constants, redis_store
+import json
 
 area_li = None
 
@@ -14,15 +15,29 @@ area_li = None
 def get_area_info():
     # 获取城区信息
     try:
+        resp_json = redis_store.get("area_json")
+    except Exception as e:
+        current_app.logger.error(e)
+    else:
+        if resp_json is not None:
+            # redis有缓存数据
+            return resp_json, 200, {"Content-Type": "application/json"}
+    try:
         area_li = Area.query.all()
     except Exception as e:
         current_app.logger.error(e)
         return jsonify(errno=RET.DBERR, errmsg="数据库错误")
     area_dict_li = []
+    # 将对象转换为字典
     for area in area_li:
         area_dict_li.append(area.to_dict())
+    # 将数据转换为json字符串
+    resp_dict = dict(errno=RET.OK, errmsg="OK", data=area_dict_li)
+    resp_json = json.dumps(resp_dict)
     # 将数据保存到redis中
     try:
-        redis_store
+        redis_store.setex("area_info", constants.AREA_INFO_REDIS_CACHE_EXPIRES, resp_json)
+    except Exception as e:
+        current_app.logger.error(e)
 
-    return jsonify(errno=RET.OK, errmsg="OK", data=area_dict_li)
+    return resp_json, 200, {"Content-Type": "application/json"}
