3c7883f54d8874f0135e9d67cd27358c1e5a51ad
浮生若梦 <microease@163.com>
2019-03-17 15:55:20 +0800

    add

--- 15期/17 爱家租房项目/ihome_python/ihome/api_1_0/houses.py
+++ 15期/17 爱家租房项目/ihome_python/ihome/api_1_0/houses.py
@@ -6,6 +6,8 @@ from ihome.utils.response_code import RET
 from ihome.utils.image_storage import storage
 from ihome.models import User, Area
 from ihome import db, constants, redis_store
+
+from ihome.utils.image_storage import storage
 import json
 
 area_li = None
@@ -99,6 +101,49 @@ def save_house_info():
     except Exception as e:
         current_app.logger.error(e)
         return jsonify(errno=RET.DBERR, errmsg="保存数据异常")
-    facilities = house_data.get("facility")
+    db.session.add(house)
+    facility_ids = house_data.get("facility")
+    if facility_ids:
+        try:
+            facilities = Facility.query.filter(Facility.id.in_(facility_ids))
+        except Exception as e:
+            current_app.logger.error(e)
+            return jsonify(error=RET.DBERR, errmsg="数据库异常")
     if facilities:
-        pass
+        # 表示有合法数据
+        house.facilities = facilities
+        try:
+            db.session.add(house)
+            db.session.commit()
+        except Exception as e:
+            current_app.logger.error(e)
+            db.session.rollback()
+            return jsonify(errno=RET.DBERR, errmsg="保存数据失败")
+        return jsonify(errno=RET.OK, errmsg="保存数据成功", data={"house_id": house.id})
+
+
+@api.route("/house/image", methods=["POST"])
+@login_required
+def save_house_image():
+    # 保存房屋的图片
+    image_file = request.files.get("house_image")
+    house_id = request.form.get("house_id")
+    if not all([image_file, house_id]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+    # 判断house_id 正确性
+    try:
+        house = House.query.get(house_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="数据库异常")
+    if house is None:
+        return jsonify(errno=RET.NODATA, errmsg="房屋不存在")
+    # 保存图片到七牛中
+    image_data = image_file.read()
+    try:
+        file_name = storage(image_data)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.THIRDERR, errmsg="保存图片失败")
+    house_image = HouseImage(house_id=house_id, url=file_name)
+    db.session.add(house_image)
