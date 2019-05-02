472021fd2b81c2bde79b91926d55fa996b9558ab
microease <microease@163.com>
2019-05-02 23:39:48 +0800

    add

--- 15期/25 Python网络爬虫/02-网络请求/test6.py
+++ 15期/25 Python网络爬虫/02-网络请求/test6.py
@@ -2,4 +2,14 @@
 # Author :       huxiaoyi
 # Date：         2019-05-02
 # Time：         20:14
-from 
+from urllib import request
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.108 Safari/537.36'
+
+}
+url = "https://www.lagou.com/zhaopin/Python/"
+
+req = request.Request(url, headers=headers)
+resp = request.urlopen(req)
+print(resp.read().decode())
---
+++ 15期/25 Python网络爬虫/02-网络请求/test7.py
@@ -0,0 +1,4 @@
+# coding:utf8
+# Author :       huxiaoyi
+# Date：         2019-05-02
+# Time：         23:39
