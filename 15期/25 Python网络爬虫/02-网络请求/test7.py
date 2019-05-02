376cc729175f9f08a31d42183d935a0d549f662e
microease <microease@163.com>
2019-05-03 07:40:09 +0800

    add

--- 15期/25 Python网络爬虫/02-网络请求/test7.py
+++ 15期/25 Python网络爬虫/02-网络请求/test7.py
@@ -3,3 +3,17 @@
 # Description :
 # Author :       huxiaoyi
 # Date：          2019-05-03
+from urllib import request
+
+url = 'http://httpbin.org/ip'
+# resp = request.urlopen(url)
+# print(resp.read())
+# b'{\n  "origin": "119.123.42.99, 119.123.42.99"\n}\n'
+handler = request.ProxyHandler({"http": "112.85.129.250:9999"})
+
+opener = request.build_opener(handler)
+
+resp = opener.open(url)
+
+print(resp.read())
+# b'{\n  "origin": "112.85.129.250, 112.85.129.250"\n}\n'
