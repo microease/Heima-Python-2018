ce46e61a6da377d8c888f353c8e209de4632b104
浮生若梦 <microease@163.com>
2019-05-06 15:25:29 +0800

    add

---
+++ 15期/21 数据分析/02-matplotlib折线图/test2.py
@@ -0,0 +1,12 @@
+# coding:utf-8
+# File Name：     test2
+# Description :
+# Author :       microease
+# Date：          2019/5/6
+
+from matplotlib import pyplot as plt
+import random
+
+a = [random.randint(20, 35) for i in range(120)]
+
+x = range(2, )
