accf418c3506a7bee3153cc79deb6a1c5b0fe267
浮生若梦 <microease@163.com>
2019-05-06 23:41:28 +0800

    add

---
+++ 15期/21 数据分析/02-matplotlib折线图/test3.py
@@ -0,0 +1,10 @@
+# coding:utf-8
+# File Name：     test3
+# Description :
+# Author :       microease
+# Date：          2019/5/6
+from mat
+
+a = [1, 0, 1, 1, 2, 4, 3, 2, 3, 4, 4, 5, 6, 5, 4, 3, 3, 1, 1, 1]
+b = range(11,30)
+
