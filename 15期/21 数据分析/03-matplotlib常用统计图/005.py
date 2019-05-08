eed56dc7568ed75aab5d8eb51079f9b98b04558d
浮生若梦 <microease@163.com>
2019-05-08 15:22:51 +0800

    add

--- 15期/21 数据分析/03-matplotlib常用统计图/005.py
+++ 15期/21 数据分析/03-matplotlib常用统计图/005.py
@@ -9,5 +9,6 @@ from matplotlib import font_manager
 interval = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 60, 90]
 width = [5, 5, 5, 5, 5, 5, 5, 5, 5, 15, 30, 60]
 quantity = [836, 2737, 3723, 3926, 3596, 1438, 3273, 642, 824, 613, 215, 57]
-
-plt.bar(range(len(quantity)), quantity, width=width)
+plt.figure(figsize=(20, 8), dpi=80)
+plt.bar(range(len(quantity)),quantity,width=1)
+plt.show()
