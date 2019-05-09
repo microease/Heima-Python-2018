a7730085627c1927e398dd0cfc747e29ab6d0e19
microease <microease@163.com>
2019-05-09 20:59:48 +0800

    add

--- 15期/21 数据分析/04-numpy数组/test1.py
+++ 15期/21 数据分析/04-numpy数组/test1.py
@@ -6,4 +6,12 @@
 import numpy as np
 
 t1 = np.arange(12)
-print(t1.shape)
+print(t1)
+print(type(t1))
+print(t1.dtype)
+
+t2 = np.array(range(1,4),dtype=float)
+print(t2)
+print(t2.dtype)
+t2 = t2.astype("int8")
+print(t2.dtype)
