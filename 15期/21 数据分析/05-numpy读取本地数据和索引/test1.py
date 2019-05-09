34502a891cb378e76790dfe47927a99f3fa7effa
浮生若梦 <microease@163.com>
2019-05-09 17:56:15 +0800

    add

--- 15期/21 数据分析/05-numpy读取本地数据和索引/test1.py
+++ 15期/21 数据分析/05-numpy读取本地数据和索引/test1.py
@@ -8,5 +8,5 @@ import numpy as np
 us_file_path = "./USvideos.csv"
 gb_file_path = "./GBvideos.csv"
 # category_id	views	likes	dislikes	comment_total
-t1 = np.loadtxt(us_file_path, encoding="utf8", delimiter=",", dtype="str", unpack=True)
+t1 = np.loadtxt(us_file_path, encoding="ISO-8859-1", delimiter=",", dtype="str", unpack=True)
 print(t1)
