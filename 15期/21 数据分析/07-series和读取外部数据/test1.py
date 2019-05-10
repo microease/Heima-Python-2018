d4fc0ea7c416a1068e5edebd95723d2873f0ea09
浮生若梦 <microease@163.com>
2019-05-10 16:15:17 +0800

    add

---
+++ 15期/21 数据分析/07-series和读取外部数据/test1.py
@@ -0,0 +1,9 @@
+# coding:utf-8
+# File Name：     test1
+# Description :
+# Author :       microease
+# Date：          2019/5/10
+import pandas as pd
+
+dog_names = pd.read_csv("./dogNames2.csv")
+print(dog_names)
